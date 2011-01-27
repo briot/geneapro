57070b72e953eb49de49147d3aa22135901052b4
Emmanuel Briot <briot.emmanuel@gmail.com>
2011-01-27 18:00:26 +0100

    Use sqlite instead of postgres as the default storage.  This is faster, and easier to setup in any case

--- settings.py
+++ settings.py
@@ -10,14 +10,15 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE='postgresql'
-DATABASE_NAME='geneapro'
+#DATABASE_ENGINE='postgresql'
+#DATABASE_NAME='geneapro'
+DATABASE_ENGINE='sqlite3'
+DATABASE_NAME='geneapro.db'
 DATABASE_USER='briot'
 DATABASE_PASSWORD=''
 DATABASE_HOST=''
 DATABASE_PORT=''
 
-#DATABASE_ENGINE = 'sqlite3'    # or 'mysql'
 #DATABASE_NAME = 'database.db'  # Or path to database file if using sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
