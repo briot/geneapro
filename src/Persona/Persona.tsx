5448d90fbf772144900e79742ed853fdf9c4df5d
Emmanuel Briot <briot.emmanuel@gmail.com>
2018-03-29 13:11:43 +0200

    Display person's lifespan and gender at the top of the persona page

--- src/Persona/Persona.tsx
+++ src/Persona/Persona.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { Icon } from 'semantic-ui-react';
 import { AppState } from '../Store/State';
+import { P2C } from '../Store/Assertion';
 import { Segment } from 'semantic-ui-react';
 import { Person } from '../Store/Person';
 import { GenealogyEventSet } from '../Store/Event';
@@ -21,11 +23,39 @@ function View(props: ConnectedProps) {
    const birthYear: number|undefined = birthEvent && birthEvent.date_sort ?
       Number(birthEvent.date_sort.substring(0, 4)) :
       undefined;
+   const deathEvent = p.deathEventId ? props.events[p.deathEventId] : undefined;
+   const deathYear: number|undefined = deathEvent && deathEvent.date_sort ?
+      Number(deathEvent.date_sort.substring(0, 4)) :
+      undefined;
+
+   let gender: string = '';
+   if (p.asserts) {
+      for (const a of p.asserts.get()) {
+         // ??? Should compare non-display string, not "sex"
+         if (a instanceof P2C && a.characteristic.name === 'sex') {
+            for (const p of a.characteristic.parts) {
+               if (p.name === 'sex') {
+                  gender = p.value;
+               }
+            }
+            break;
+         }
+      }
+   }
 
    return (
       <div className="Persona">
          <Segment attached={true} className="pagetitle">
+            <Icon
+               className="gender"
+               name={gender === 'M' ? 'man' :
+                     gender === 'F' ? 'woman' :
+                    'genderless'}
+            />
             {p.surn.toUpperCase()}{' '}{p.givn}
+            <span className="lifespan">
+               {birthYear} - {deathYear}
+            </span>
          </Segment>
          <Segment attached={true} className="pageContent">
             <AssertionTimeline
