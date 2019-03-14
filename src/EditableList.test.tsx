61205057b673e246892fb6cdd775b088ce89cc22
Emmanuel Briot <briot.emmanuel@gmail.com>
2019-03-14 10:15:53 +0100

    Initial test for EditableList
    As a way to experiment with React tests

---
+++ src/EditableList.test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
+import EditableList from './EditableList';
+// Some support in setupTests
+
+it('renders all items in the list', () => {
+   const list = [1, 2, 3];
+
+   // General notes on testing with create-react-app:
+   //    https://facebook.github.io/create-react-app/docs/running-tests
+
+   // List of attributes returned from render():
+   //    https://testing-library.com/docs/react-testing-library/api
+   const { container, debug, queryAll } = render(
+      <EditableList
+         list={list}
+         render={a => <span>{a}</span>}
+         create={() => 4}
+         onChange={newlist => null}
+         orderable={false}
+      />
+   );
+   //debug();
+
+   // List of possible Jest tests:
+   //    https://jestjs.io/docs/en/expect.html#content
+   // Some more tests with jest-dom:
+   //    https://github.com/gnapse/jest-dom
+   expect(container.querySelectorAll('div.listItem')).toHaveLength(3);
+
+   expect(container.querySelectorAll('[draggable=true]')).toHaveLength(0);
+});
